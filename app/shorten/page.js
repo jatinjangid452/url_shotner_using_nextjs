bc4305db34f3f1ce23e8308845701189984e82de
jatin jangif <jatin.j@lucidsolutions.in>
2025-08-28 11:58:00 +0530

    commit code

--- app/shorten/page.js
+++ app/shorten/page.js
@@ -1,3 +1,4 @@
+
 "use client"
 import Link from 'next/link'
 import React, { useState } from 'react'
@@ -5,63 +6,60 @@ import React, { useState } from 'react'
 const Shorten = () => {
     const [url, seturl] = useState("")
     const [shorturl, setshorturl] = useState("")
-    const [generated, setgenerated] = useState("")
+    const [generated, setGenerated] = useState("")
 
-    const generate = (first) => { 
-      const myHeaders = new Headers();
-      myHeaders.append("Content-Type", "application/json");
-      myHeaders.append("Cookie", "CFID=6604; CFTOKEN=65d31556360d889f-511309C0-BEA7-D2D7-6DE1C2469C77393B");
+    const generate = () => {
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
 
-      const raw = JSON.stringify({
-        "url": url,
-        "shorturl": shorturl
-      });
+        const raw = JSON.stringify({
+            "url": url,
+            "shorturl": shorturl
+        });
 
-      const requestOptions = {
-        method: "POST",
-        headers: myHeaders,
-        body: raw,
-        redirect: "follow"
-      };
+        const requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow"
+        };
 
-      fetch("/api/generate", requestOptions)
-        .then((response) => response.json())
-        .then((result) => { 
-          setgenerated(`${process.env.NEXT_PUBLIC_HOST}/${shorturl}`);
-          seturl("");
-          setshorturl("");
-          console.log(result)
-          alert(result.message)
-        })
-        .catch((error) => console.error(error));
-     }
-  return (
-    <div className='mx-auto max-w-lg bg-purple-100 my-16 p-8 gap-4 rounded-lg flex flex-col'>
-      <h1 className='font-bold text-2xl'>Generate your short URLs</h1>
-      <div className='flex flex-col gap-2'>
-        <input type="text" 
-          className='px-4 py-2 focus:outline-purple-600 rounded-md'
-          value={ url }
-          placeholder='Enter your URL'
-          onChange={e =>{ seturl(e.target.value) }}/>
+        fetch("/api/generate", requestOptions)
+            .then((response) => response.json())
+            .then((result) => {
+                setGenerated(`${process.env.NEXT_PUBLIC_HOST}/${shorturl}`)
+                seturl("")   
+                setshorturl("")
+                console.log(result)
+                alert(result.message)
+            
+            })
+            .catch((error) => console.error(error));
+    }
 
-        <input type="text" 
-          className='px-4 py-2 focus:outline-purple-600 rounded-md'
-          value={ shorturl }
-          placeholder='Enter your preferred short URL text' 
-          onChange={e =>{ setshorturl(e.target.value) }}/>
 
-        <button onClick={generate} className='bg-purple-500 shadow-lg p-3 rounded-lg py-1 my-3 font-bold text-white'>
-          Generate
-        </button>
-      </div>
+    return (
+        <div className='mx-auto max-w-lg bg-purple-100 my-16 p-8 rounded-lg flex flex-col gap-4'>
+            <h1 className='font-bold text-2xl'>Generate your short URLs</h1>
+            <div className='flex flex-col gap-2'>
+                <input type="text"
+                    value={url}
+                    className='px-4 py-2 focus:outline-purple-600 rounded-md'
+                    placeholder='Enter your URL'
+                    onChange={e => { seturl(e.target.value) }} />
 
-      {generated && 
-      <><span className='font-bold text-lg'>Your Link :</span> <code><Link target='_blank' href={generated}>{generated}</Link> 
-        </code>
-        </>}
-    </div>
-  )
+                <input type="text"
+                    value={shorturl}
+                    className='px-4 py-2 focus:outline-purple-600 rounded-md'
+                    placeholder='Enter your preferred short URL text'
+                    onChange={e => { setshorturl(e.target.value) }} />
+                <button onClick={generate} className='bg-purple-500 rounded-lg shadow-lg p-3 py-1 my-3 font-bold text-white'>Generate</button>
+            </div>
+
+            {generated && <> <span className='font-bold text-lg'>Your Link </span><code><Link target="_blank" href={generated}>{generated}</Link> 
+                </code></>}
+        </div>
+    )
 }
 
 export default Shorten
